66777d52dae63b0133cca1745fff2d622dd06448
Cuong Nguyen <manhcuongit1010@gmail.com>
2017-12-22 18:53:52 +0700

    sửa db, sua file edmx.sql, nhớ tao db la QLTour va sua lai link trong app.config nha

--- QLTour/WinTour/DATA/Database.cs
+++ QLTour/WinTour/DATA/Database.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusAndModel
+namespace WinTour.DATA
 {
-    public class Class1
+    class Database
     {
     }
 }
